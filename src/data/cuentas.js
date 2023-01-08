15df552e0d779fb30ddced0348111a3ca9b72946
Mateo Gomez <mateogomezmolina@gmail.com>
2023-01-07 23:26:44 -0500

    Add provisional data

---
+++ src/data/cuentas.js
@@ -0,0 +1,31 @@
+export default {
+    income: 3000000,
+    outcome: 2000000,
+    balance: 1000000,
+
+    accounts: ["Todas las cuentas", "bancolombia", "rappicard"],
+
+    bancolombia: [
+        {
+            timestamp: 1673139385096,
+            value: 200000,
+            type: "outcome",
+            category: "Mercado",
+            description: "Mercado del mes",
+        },
+        {
+            timestamp: 1673139385034,
+            value: 100000,
+            type: "outcome",
+            category: "Ropa",
+            description: "Camiseta",
+        },
+        {
+            timestamp: 1673139382345,
+            value: 1500000,
+            type: "income",
+            category: "Nómina",
+            description: "Salario",
+        },
+    ],
+}
