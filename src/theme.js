8cfa5e252f4d6945a2b198c100526ed4c4dd9145
Mateo Gomez <mateogomezmolina@gmail.com>
2023-01-07 23:20:52 -0500

    Add theme

---
+++ src/theme.js
@@ -0,0 +1,29 @@
+export const theme = {
+    fontSizes: {
+        normal: 12,
+        big: 20,
+        small: 10,
+    },
+    fontWeights: {
+        bold: "bold",
+        100: 100,
+        300: 300,
+        500: 500,
+        700: 700,
+        900: 900,
+    },
+    colors: {
+        secondary: "#00CCBF",
+        primary: "#3F7C85",
+        greenLight: "#72F2EB",
+        grey: "#747E7E",
+        red: "#FF5F5D",
+    },
+    flex: {
+        center: "center",
+        end: "flex-end",
+        start: "flex-start",
+        between: "flex-between",
+        around: "flex-around",
+    },
+}
