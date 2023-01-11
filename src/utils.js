0221cc56df600e19e28d1050f4f2a1173474c186
Mateo Gomez <mateogomezmolina@gmail.com>
2023-01-10 20:52:44 -0500

    Modify formating in currecy util

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,7 @@
 const intlCurrency = new Intl.NumberFormat("es-CO", {
-    style: "currency",
-    currency: "COP",
     maximumSignificantDigits: 2,
-    currencyDisplay: "code",
 })
 
 export const formatNumber = (value) => {
-    return intlCurrency.format(value)
+    return intlCurrency.format(value) + " COP"
 }
