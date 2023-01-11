321c98cc089a59e41c78cbe8eba509f1ee3f18ca
Mateo Gomez <mateogomezmolina@gmail.com>
2023-01-10 20:55:14 -0500

    Add transaction adder buttons

--- src/Components/Transactions.jsx
+++ src/Components/Transactions.jsx
@@ -1,12 +1,26 @@
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, TouchableOpacity, View } from "react-native"
 import cuentasData from "../data/cuentas"
 import StyledText from "./StyledText"
 import NumberFormat from "./NumberFormat"
 import TransactionList from "./TransactionList"
+import { useNavigate } from "react-router-native"
+import PlusIcon from "./PlusIcon"
+import { theme } from "../theme"
+import MinusCircle from "./MinusCircle"
 
 const Transactions = () => {
+    const navigate = useNavigate()
+
+    const handlePressPlusButton = () => {
+        navigate("/transactions/add")
+    }
+
+    const handlePressMinusButton = () => {
+        navigate("/transactions/add")
+    }
+
     return (
-        <View>
+        <View style={{ flex: 1 }}>
             <View style={styles.center}>
                 <StyledText
                     fontWeight="bold"
@@ -28,7 +42,15 @@ const Transactions = () => {
                     <NumberFormat big bold value={cuentasData.balance} />
                 </View>
 
-                <TransactionList cuentas={cuentasData.bancolombia} />
+                <TransactionList cuentas={cuentasData.transactions} />
+            </View>
+            <View style={styles.buttons}>
+                <TouchableOpacity onPress={handlePressPlusButton}>
+                    <PlusIcon color={theme.colors.greenLight} size={150} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={handlePressMinusButton}>
+                    <MinusCircle color={theme.colors.red} size={150} />
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -45,6 +67,13 @@ const styles = StyleSheet.create({
     },
     list: {
         marginBottom: 10,
+        flex: 1,
+    },
+    buttons: {
+        paddingVertical: 20,
+        alignItems: "center",
+        justifyContent: "space-around",
+        flexDirection: "row",
     },
 })
 
