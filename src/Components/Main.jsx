5515f8bead365b7ef5c329c9264ce78b040ee512
Mateo Gomez <mateogomezmolina@gmail.com>
2023-01-10 20:56:21 -0500

    Implement routes with react router native

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,18 +1,24 @@
 import { StyleSheet, View } from "react-native"
 import Transactions from "./Transactions"
 import AppBar from "./AppBar"
+import { Route, Routes } from "react-router-native"
+import AddTransaction from "./AddTransaction"
 
 const Main = () => {
     return (
         <View style={styles.container}>
             <AppBar />
-            <Transactions />
+            <Routes>
+                <Route path="/" element={<Transactions />} />
+                <Route path="/transactions/add" element={<AddTransaction />} />
+            </Routes>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         flexGrow: 1,
     },
 })
