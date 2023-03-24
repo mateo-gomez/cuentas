6dd39482b70a8ba6312aa9ac42a3a67bfdeda80a
Mateo Gomez <mateogomezmolina@gmail.com>
2023-03-23 20:54:22 -0500

    Add fetch categories

--- src/Pages/CategoriesList.jsx
+++ src/Pages/CategoriesList.jsx
@@ -1,7 +1,11 @@
 import { Dimensions, FlatList, View } from "react-native"
-import categories from "../data/categories"
 import Category from "../Components/Category"
 import { useOutletContext } from "react-router-native"
+import { useEffect, useState } from "react"
+import Constants from "expo-constants"
+import StyledText from "../Components/StyledText"
+
+const { apiUrl } = Constants.expoConfig.extra
 
 const CategoriesList = () => {
     const gap = 10
@@ -12,7 +16,31 @@ const CategoriesList = () => {
 
     const { handleSelectCategory } = useOutletContext()
 
-    return (
+    const [categories, setCategories] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const getCategories = async () => {
+            setLoading(true)
+
+            try {
+                const response = await fetch(`${apiUrl}/categories`)
+                const data = await response.json()
+
+                setCategories(data)
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        getCategories()
+    }, [])
+
+    return loading ? (
+        <StyledText textCenter>Cargando...</StyledText>
+    ) : (
         <FlatList
             numColumns={numColumns}
             keyExtractor={({ id }) => id}
@@ -20,6 +48,9 @@ const CategoriesList = () => {
             horizontal={false}
             key={numColumns}
             ItemSeparatorComponent={<View style={{ marginBottom: 10 }} />}
+            ListEmptyComponent={
+                <StyledText textCenter>Sin categorías</StyledText>
+            }
             renderItem={({ item: category, index }) => (
                 <Category
                     size={categoryBoxSize}
