f9518005c75e83047b43af7e9d2eb17dfbb4fde0
Mateo Gomez <mateogomezmolina@gmail.com>
2024-02-23 15:21:49 -0500

    fix: highlight selected record category

--- cuentas/src/Pages/Categories.tsx
+++ cuentas/src/Pages/Categories.tsx
@@ -1,13 +1,30 @@
 import { useOutletContext } from "react-router-native"
 import CategoryList from "../Components/CategoryList"
 import { useCategories } from "../hooks/useCategories"
+import { Category } from "./category/types"
+
+interface UseOutletContext {
+  handleSelectCategory: (category: Category) => void
+  categoryId: string
+}
 
 const Categories = () => {
-  const { handleSelectCategory } = useOutletContext()
+  const { handleSelectCategory, categoryId } =
+    useOutletContext<UseOutletContext>()
   const { categories } = useCategories()
 
+  const currentCategory = categories.find(
+    (category) => category._id === categoryId,
+  )
+  console.log({ selection: currentCategory, categoryId })
   return (
-    <CategoryList categories={categories} onSelect={handleSelectCategory} />
+    <CategoryList
+      highlightCriteria={(category) =>
+        currentCategory && category._id === currentCategory._id
+      }
+      categories={categories}
+      onSelect={handleSelectCategory}
+    />
   )
 }
 
