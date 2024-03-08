488da515a70ba916c2c9a03f4d265848f933f87e
Mateo Gomez <mateogomezmolina@gmail.com>
2024-03-08 18:33:05 -0500

    fix: not default value for PORT var

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -2,5 +2,5 @@ const envvars = Deno.env.toObject();
 
 export default {
   MONGO_URI: envvars.MONGO_URI,
-  PORT: (envvars.PORT || 8000) as unknown as number,
+  PORT: envvars.PORT as unknown as number,
 };
