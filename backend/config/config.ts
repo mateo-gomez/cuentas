c908408cb6d640ef5426a5aa86cf37c79b8ca95a
Mateo Gomez <mateogomezmolina@gmail.com>
2024-03-08 16:41:08 -0500

    chore: default mongo uri

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -4,6 +4,6 @@ const loadConfig = await load();
 
 export default {
   MONGO_URI: (loadConfig.MONGO_URI ||
-    "mongodb://localhost:27017/cuentas") as unknown as string,
+    "") as unknown as string,
   PORT: (loadConfig.MONGO_URI || 8000) as unknown as number,
 };
