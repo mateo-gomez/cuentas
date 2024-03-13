9cc69a401d2b304c38915825b167a34ad704954d
Mateo Gomez <mateogomezmolina@gmail.com>
2024-03-12 22:51:39 -0500

    fix: add safe area to view and fix status bar color

--- cuentas/App.tsx
+++ cuentas/App.tsx
@@ -1,10 +1,15 @@
 import { NativeRouter } from "react-router-native"
 import Routes from "./src/Routes"
+import { StatusBar } from "expo-status-bar"
+import { SafeAreaProvider } from "react-native-safe-area-context"
 
 export default function App() {
   return (
     <NativeRouter>
-      <Routes />
+      <SafeAreaProvider>
+        <StatusBar style="light" />
+        <Routes />
+      </SafeAreaProvider>
     </NativeRouter>
   )
 }
