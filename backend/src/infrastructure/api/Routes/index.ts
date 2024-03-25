5d178a5d8764b57574d84dad6cddbc53a0b6101b
Mateo Gomez <mateogomezmolina@gmail.com>
2024-03-25 13:59:12 -0500

    feat: import all exported routes from dir

---
+++ backend/src/infrastructure/api/Routes/index.ts
@@ -0,0 +1,17 @@
+import { path, Router } from "../../../../deps.ts";
+
+const dirPath = path.dirname(path.fromFileUrl(import.meta.url));
+const router = new Router();
+
+for await (const dirEntry of Deno.readDir(dirPath)) {
+  if (dirEntry.name !== "index.ts") {
+    const filePath = import.meta.resolve(`${dirPath}/${dirEntry.name}`);
+    const routerFile = await import(filePath);
+    const routes: Router = routerFile.default;
+
+    router.use(routes.routes());
+    router.use(routes.allowedMethods());
+  }
+}
+
+export default router;
--- backend/src/infrastructure/api/app.ts
+++ backend/src/infrastructure/api/app.ts
@@ -1,13 +1,12 @@
 import config from "../../../config/config.ts";
-import { ListenOptionsBase, Router } from "../../../deps.ts";
+import { ListenOptionsBase } from "../../../deps.ts";
 import { RequestLogger } from "./middlewares/RequestLogger.ts";
 import { TimeMiddleware } from "./middlewares/TimeMiddleware.ts";
 import { ErrorHandler } from "./middlewares/errorHandler.ts";
 import { Api } from "./server.ts";
 import { getIPAddress } from "./utils/getIPAddress.ts";
-import categoriesRouter from "./Routes/categories.routes.ts";
-import transactionsRouter from "./Routes/transactions.routes.ts";
 import { DBMongo } from "../database/db.ts";
+import routes from "./Routes/index.ts";
 
 (async () => {
   const hostname = getIPAddress();
@@ -17,15 +16,9 @@ import { DBMongo } from "../database/db.ts";
     new TimeMiddleware(),
     new ErrorHandler(),
   ];
-  const mainRouter = new Router();
-
-  mainRouter.use(categoriesRouter.routes());
-  mainRouter.use(categoriesRouter.allowedMethods());
-  mainRouter.use(transactionsRouter.routes());
-  mainRouter.use(transactionsRouter.allowedMethods());
 
   const db = new DBMongo(Deno.env.get("MONGO_URI") || "");
-  const app = new Api({ listenOptions, middlewares, routers: mainRouter });
+  const app = new Api({ listenOptions, middlewares, routes });
 
   await db.connect();
   await app.run();
