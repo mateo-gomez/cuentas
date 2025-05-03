6979ce38adef700ff2edac2aafcacb60daf8ec29
Mateo Gomez <mateogomezmolina@gmail.com>
2025-05-03 16:19:30 -0500

    fix: errorHandler middleware

--- backend/src/infrastructure/api/middlewares/errorHandler.ts
+++ backend/src/infrastructure/api/middlewares/errorHandler.ts
@@ -1,3 +1,4 @@
+import { ErrorRequestHandler } from "express";
 import { ApplicationError } from "../../../application/errors/applicationError";
 import { ForbiddenError } from "../../../application/errors/forbiddenError";
 import { NotFoundError } from "../../../application/errors/notFoundError";
@@ -8,60 +9,57 @@ import { Status } from "../status";
 import { Middleware, NextFunction, Request, Response } from "./BaseMiddleware";
 
 export class ErrorHandler {
-	async execute(
-		error: Error,
-		_request: Request,
-		response: Response,
-		next: NextFunction
-	) {
-		console.error(error);
+	static handle(): ErrorRequestHandler {
+		return (
+			error: Error,
+			_request: Request,
+			response: Response,
+			next: NextFunction
+		) => {
+			console.error("error", error);
+			if (response.headersSent) {
+				next(error);
+				return;
+			}
 
-		if (response.headersSent) {
-			next(error);
-			return;
-		}
+			if (error instanceof ApplicationError) {
+				const res = HttpResponse.failed(error.message);
+				response.status(res.statusCode).json(res);
+				return;
+			}
 
-		if (error instanceof ApplicationError) {
-			const res = HttpResponse.failed(error.message);
-			response.status(res.statusCode).json(res);
-			return;
-		}
+			if (error instanceof ValidationError) {
+				const res = HttpResponse.validationFailed(error.errors);
+				response.status(res.statusCode).json(res);
+				return;
+			}
 
-		if (error instanceof ValidationError) {
-			const res = HttpResponse.validationFailed(error.errors);
-			response.status(res.statusCode).json(res);
-			return;
-		}
+			if (error instanceof NotFoundError) {
+				const res = HttpResponse.failed(error.message, 404);
+				response.status(res.statusCode).json(res);
+				return;
+			}
 
-		if (error instanceof NotFoundError) {
-			const res = HttpResponse.failed(error.message, 404);
-			response.status(res.statusCode).json(res);
-			return;
-		}
+			if (error instanceof ForbiddenError) {
+				const res = HttpResponse.failed(error.message, 403);
+				response.status(res.statusCode).json(res);
+				return;
+			}
 
-		if (error instanceof ForbiddenError) {
-			const res = HttpResponse.failed(error.message, 403);
-			response.status(res.statusCode).json(res);
-			return;
-		}
+			if (error instanceof HttpNotFoundError) {
+				const res = HttpResponse.failed(error.message, error.statusCode);
+				response.status(res.statusCode).json(res);
+				return;
+			}
 
-		if (error instanceof HttpNotFoundError) {
-			const res = HttpResponse.failed(error.message, error.statusCode);
-			response.status(res.statusCode).json(res);
-			return;
-		}
-
-		if (error instanceof Error) {
-			const res = HttpResponse.failed(
-				"Error interno del servidor",
-				Status.InternalServerError
-			);
-			response.status(res.statusCode).json(res);
-			return;
-		}
+			if (error instanceof Error) {
+				const res = HttpResponse.failed(
+					"Error interno del servidor",
+					Status.InternalServerError
+				);
+				response.status(res.statusCode).json(res);
+				return;
+			}
+		};
 	}
-
-	static handle = () => {
-		return new this().execute;
-	};
 }
